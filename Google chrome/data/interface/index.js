2522f2d05a600c93b589cd34d283b6530c4917fd
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2023-04-30 17:01:12 +0530

    for

--- Google chrome/data/interface/index.js
+++ Google chrome/data/interface/index.js
@@ -36,56 +36,56 @@ async function hydrate() {
   const enabled = await a;
   console.log(enabled);
 
-  var c = new Promise(function(resolve, reject){
-        chrome.storage.sync.get({"videoCount": 0}, function(options){
-            resolve(options.videoCount);
-        })
-    });
+  // var c = new Promise(function(resolve, reject){
+  //       chrome.storage.sync.get({"videoCount": 0}, function(options){
+  //           resolve(options.videoCount);
+  //       })
+  //   });
 
-  const videoCount = await c;
-  console.log(videoCount);
+  // const videoCount = await c;
+  // console.log(videoCount);
 
-  // Hydrate Logo
-  const $logo = document.querySelector(".logo");
-  $logo.style.filter = enabled ? "grayscale(0)" : "grayscale(100%)";
-  $logo.style.opacity = enabled ? "1" : "0.7";
+  // // Hydrate Logo
+  // const $logo = document.querySelector(".logo");
+  // $logo.style.filter = enabled ? "grayscale(0)" : "grayscale(100%)";
+  // $logo.style.opacity = enabled ? "1" : "0.7";
 
-  // Hydrate Timesave info
-  const $timeSaveInfo = document.querySelector(".timesave-info");
-  const adTimePerVideo = 0.5;
-  const timeSaved = Math.ceil(videoCount * adTimePerVideo);
-  $timeSaveInfo.textContent = chrome.i18n.getMessage("timesaveInfo", [
-    new Intl.NumberFormat(undefined, {
-      style: "unit",
-      unit: "minute",
-      unitDisplay: "long",
-    }).format(timeSaved),
-  ]);
+  // // Hydrate Timesave info
+  // const $timeSaveInfo = document.querySelector(".timesave-info");
+  // const adTimePerVideo = 0.5;
+  // const timeSaved = Math.ceil(videoCount * adTimePerVideo);
+  // $timeSaveInfo.textContent = chrome.i18n.getMessage("timesaveInfo", [
+  //   new Intl.NumberFormat(undefined, {
+  //     style: "unit",
+  //     unit: "minute",
+  //     unitDisplay: "long",
+  //   }).format(timeSaved),
+  // ]);
 
-  // Hydrate Checkbox Label
-  const $checkboxLabel = document.querySelector("[data-message=enabled]");
-  $checkboxLabel.textContent = chrome.i18n.getMessage(
-    enabled ? "enabled" : "disabled"
-  );
+  // // Hydrate Checkbox Label
+  // const $checkboxLabel = document.querySelector("[data-message=enabled]");
+  // $checkboxLabel.textContent = chrome.i18n.getMessage(
+  //   enabled ? "enabled" : "disabled"
+  // );
 
-  // Hydrate Checkbox Label
-  const $enabledCheckbox = document.querySelector("input[name=enabled]");
-  $enabledCheckbox.checked = enabled;
-  $enabledCheckbox.addEventListener("change", async (event) => {
-    const enabled = event.currentTarget.checked;
+  // // Hydrate Checkbox Label
+  // const $enabledCheckbox = document.querySelector("input[name=enabled]");
+  // $enabledCheckbox.checked = enabled;
+  // $enabledCheckbox.addEventListener("change", async (event) => {
+  //   const enabled = event.currentTarget.checked;
 
-    // Persist
-    await chrome.storage.sync.set({ enabled });
+  //   // Persist
+  //   await chrome.storage.sync.set({ enabled });
 
-    // Update Checkbox Label
-    $checkboxLabel.textContent = chrome.i18n.getMessage(
-      enabled ? "enabled" : "disabled"
-    );
+  //   // Update Checkbox Label
+  //   $checkboxLabel.textContent = chrome.i18n.getMessage(
+  //     enabled ? "enabled" : "disabled"
+  //   );
 
-    // Update Logo
-    $logo.style.filter = enabled ? "grayscale(0)" : "grayscale(100%)";
-    $logo.style.opacity = enabled ? "1" : "0.7";
-  });
+  //   // Update Logo
+  //   $logo.style.filter = enabled ? "grayscale(0)" : "grayscale(100%)";
+  //   $logo.style.opacity = enabled ? "1" : "0.7";
+  // });
 }
 
 init();
